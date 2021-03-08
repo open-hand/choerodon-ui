70a1a3cc1b8d0b1b648e944545476d923bb80b13
sunchir <gxc685@163.com>
2021-03-08 16:11:24 +0800

    [FIX] <pro>Tooltip: Fixed the component reporting error when children is undefined

--- components-pro/tooltip/Tooltip.tsx
+++ components-pro/tooltip/Tooltip.tsx
@@ -2,6 +2,7 @@ import React, { Children, Component, isValidElement } from 'react';
 import PropTypes from 'prop-types';
 import { getProPrefixCls } from 'choerodon-ui/lib/configure';
 import noop from 'lodash/noop';
+import isNil from "lodash/isNil";
 import Trigger, { TriggerProps } from '../trigger/Trigger';
 import { Action } from '../trigger/enum';
 import getPlacements, { AdjustOverflow } from './placements';
@@ -223,18 +224,19 @@ export default class Tooltip extends Component<TooltipProps, any> {
       prefixCls,
       props: { children, placement, theme, onHiddenChange, trigger, defaultHidden, hidden, ...restProps },
     } = this;
-    const child = Children.map(children, node => (
-      node && getDisabledCompatobleChildren(
+    // 修复特殊情况为0，以及 undefined 出现的报错情况
+    const child = Children.count(children) ? Children.map(children, node => (
+      !isNil(node) && getDisabledCompatobleChildren(
         isValidElement(node) ? node : <span key={`text-${node}`}>{node}</span>,
       )
-    ));
+    )) : null;
 
     const extraProps: TriggerProps = { ...restProps };
     if ('hidden' in this.props) {
       extraProps.popupHidden = hidden;
     }
     const content = this.getContent();
-    return content ? (
+    return content && !isNil(child) ? (
       <Trigger
         prefixCls={prefixCls}
         action={trigger}
