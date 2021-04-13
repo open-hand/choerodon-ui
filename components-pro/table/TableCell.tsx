322657a7e34cd28b4d5c5b6045c8080856857391
huihuawk <huihua_wk@163.com>
2021-04-13 17:39:40 +0800

    [FIX] <pro>Table: Fix the problem that the input number will not be displayed in a new line when rowHeight:auto.

--- components-pro/table/TableCell.tsx
+++ components-pro/table/TableCell.tsx
@@ -568,6 +568,8 @@ export default class TableCell extends Component<TableCellProps> {
           }
         };
       }
+    } else {
+      innerClassName.push(`${prefixCls}-inner-auto-height`);
     }
     const height = record.getState(`__column_resize_height_${name}`);
     if (height !== undefined && rows === 0) {
