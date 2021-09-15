95cf6d3c923c234eec9762fc2b7d117debabc25b
HughHzWu <99571603@qq.com>
2021-09-15 14:14:48 +0800

    [FIX] <pro>Button: Fix the problem that Tooltip will be displayed even if the content is overflow when the tooltip is overflow. <pro>Select: Fix placeholder is also diplayed in multiple mode if value is not empty. <pro>Lov: Fix the problem that the onClick property of button mode has no effect.

--- components-pro/_util/measureTextWidth.tsx
+++ components-pro/_util/measureTextWidth.tsx
@@ -3,19 +3,20 @@ import { CSSProperties } from 'react';
 export default function measureTextWidth(text: string, style?: CSSProperties | CSSStyleDeclaration) {
   if (typeof window !== 'undefined') {
     const span = document.createElement('span');
-    span.style.cssText = 'position: absolute;top: -9999px;';
+    span.style.cssText = 'position: absolute;top: -9999px;display: inline-block';
     span.innerHTML = text.replace(/\s/g, '&nbsp;');
     if (style) {
-      ['fontSize', 'fontFamily', 'fontWeight', 'letterSpacing', 'wordSpacing'].forEach((property) => {
+      ['font', 'letterSpacing', 'wordSpacing'].forEach((property) => {
         if (property in style) {
           span.style[property] = style[property];
         }
       });
     }
     document.body.appendChild(span);
-    const width = span.offsetWidth;
+    const { width } = getComputedStyle(span);
+    const contentWidth = width && width !== 'auto' ? parseFloat(width) : span.offsetWidth;
     document.body.removeChild(span);
-    return width;
+    return contentWidth;
   }
   return 0;
 }
