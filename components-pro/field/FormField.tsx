50e3dfa2ca65ec4d264b1300ef6741b5d29416bc
HughHzWu <99571603@qq.com>
2021-10-09 16:13:25 +0800

    [FIX] <pro>Attachment: Fix the problem caused by Dataset memory optimization.

--- components-pro/field/FormField.tsx
+++ components-pro/field/FormField.tsx
@@ -1285,7 +1285,7 @@ export class FormField<T extends FormFieldProps = FormFieldProps> extends DataSe
           onChange(value, old, formNode);
           this.afterSetValue();
         });
-        if (isPromise<boolean>(beforeChange)) {
+        if (isPromise(beforeChange)) {
           this.value = value;
           const rejectCallback = () => {
             this.value = storedValue;
@@ -1379,7 +1379,7 @@ export class FormField<T extends FormFieldProps = FormFieldProps> extends DataSe
       } else {
         const { record, name } = this;
         if (record && name) {
-          const field = record.getField(name)!;
+          const field = record.ownerFields.get(name) || record.addField(name);
           validator = new Validator(field);
           field.validator = validator;
         } else {
