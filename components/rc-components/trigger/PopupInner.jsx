b3a776a226bfccb7a374c5464267cf7899bd15c0
HughHzWu <99571603@qq.com>
2022-01-28 18:05:58 +0800

    [FIX] RcTrigger: Fix some problem.

--- components/rc-components/trigger/PopupInner.jsx
+++ components/rc-components/trigger/PopupInner.jsx
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import LazyRenderBox from './LazyRenderBox';
 
-const PopupInner = function PopupInner(props) {
-  let className = props.className;
-  if (props.hidden) {
-    className += ` ${props.hiddenClassName}`;
+export default class PopupInner extends Component {
+  static displayName = 'RcPopupInner';
+
+  render() {
+    const props = this.props;
+    let className = props.className;
+    if (props.hidden) {
+      className += ` ${props.hiddenClassName}`;
+    }
+    return (
+      <div
+        className={className}
+        onMouseEnter={props.onMouseEnter}
+        onMouseLeave={props.onMouseLeave}
+        style={props.style}
+        ref={props.innerRef}
+      >
+        <LazyRenderBox className={`${props.prefixCls}-content`} hidden={props.hidden}>
+          {props.children}
+        </LazyRenderBox>
+      </div>
+    );
   }
-  return (
-    <div
-      className={className}
-      onMouseEnter={props.onMouseEnter}
-      onMouseLeave={props.onMouseLeave}
-      style={props.style}
-      ref={props.innerRef}
-    >
-      <LazyRenderBox className={`${props.prefixCls}-content`} hidden={props.hidden}>
-        {props.children}
-      </LazyRenderBox>
-    </div>
-  );
-};
-
-PopupInner.displayName = 'RcPopupInner';
-
-export default PopupInner;
+}
