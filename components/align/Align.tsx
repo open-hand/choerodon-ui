b686a5057041c016d4c5f3ac99612189a6e5e16e
xilang <1139869214@qq.com>
2022-07-14 19:09:47 +0800

    [FIX] <pro>Tooltip: Fixed the problem of error reporting in tooltip zoom mode.

--- components/align/Align.tsx
+++ components/align/Align.tsx
@@ -104,7 +104,7 @@ export default class Align extends Component<AlignProps, any> {
         // If source element size changed
         const preRect = this.sourceRect;
         if (!reAlign && sourceRect
-          && (!preRect || (preRect && (preRect.width !== sourceRect.width || preRect.height !== sourceRect.height)))) {
+          && (!preRect || (preRect && (Math.round(preRect.width) !== Math.round(sourceRect.width) || Math.round(preRect.height) !== Math.round(sourceRect.height))))) {
           reAlign = true;
         }
       }
